3d195229be92aababb78225594accc698faade36
JoseOkume35 <146859473+JoseOkume35@users.noreply.github.com>
2024-10-17 14:56:25 +0000

    .

---
+++ listaContactos/Contacto.java
@@ -0,0 +1,19 @@
+package listaContactos;
+    public class Contacto{
+        private string nombres;
+        private string apellido;
+        private string direccion;
+        private string correo;
+        private string telefono;
+        private string celular;
+
+        public Contacto(String nombres, String apellido, string direccion, string correo, string telefono, string celular){
+            this.nombres = nombres;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.correo = correo;
+            this.telefono = telefono;
+            this.celular = celular;
+        }
+
+    }
---
+++ listaContactos/ListaDeContactos.java
@@ -0,0 +1,19 @@
+package listaContactos;
+import Lista.Lista;
+import Lista.PosicionIlegalExeption; 
+
+public class listaContactos {
+    //Metodos
+    //Constructor
+    private Lista<Contacto>Contactos;
+}
+
+public Lista<Contactos>mostrarTodosLosContactos()  {
+        return contactos;
+    
+}
+
+public boolean agregarContacto(String nombres, string apellidos, string direccion, string correo, string telefono,string celular) 
+throws PosicionIlegalExeption{
+    
+}
