2157cc8965323a9af1112a1331a117deca5e8653
JoseOkume35 <146859473+JoseOkume35@users.noreply.github.com>
2024-10-21 15:04:44 +0000

    .

--- listaContactos/ClienteMain.java
+++ listaContactos/ClienteMain.java
@@ -8,8 +8,8 @@ public class ClienteMain {
         try{
             ldc.agregarContacto("Fernando", "Castro", "Laguna de camano 150 Ote. Las quintas. culiacan. sinaloa",
              "FernandoC@gmail.com", "667788987");
-             ldc.agregarContacto("Fernando", "Castro", "Laguna de camano 150 Ote. Las quintas. culiacan. sinaloa",
-             "FernandoC@gmail.com", "667788987");           
+             ldc.agregarContacto("Maria","Vargas","Isla del Socorro 1085, col.las Vegas, Culiacan, Sinaloa",
+             "mariav@gmail.com","667160732","6672514532" );      
         }catch(PosicionIlegalException e){
             e.printStackTrace();
         }
